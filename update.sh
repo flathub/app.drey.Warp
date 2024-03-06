6b0c12875e45def0b3ced550ba89552e3eef083b
Fina Wilke <code@felinira.net>
2024-03-06 19:48:31 +0100

    Version 0.7.0-beta.1

--- update.sh
+++ update.sh
@@ -2,9 +2,10 @@
 git pull --rebase 'https://github.com/flathub/app.drey.Warp.git' -Xtheirs beta
 
 GITLAB_PROJECT_ID=19921
+REF=${1:-"main"}
 
-wget -O app.drey.Warp.yaml https://gitlab.gnome.org/World/warp/-/raw/main/build-aux/app.drey.Warp.yaml?inline=false
-wget -O cargo-sources.json https://gitlab.gnome.org/World/warp/-/raw/main/build-aux/cargo-sources.json?inline=false
+wget -O app.drey.Warp.yaml https://gitlab.gnome.org/World/warp/-/raw/$REF/build-aux/app.drey.Warp.yaml?inline=false
+wget -O cargo-sources.json https://gitlab.gnome.org/World/warp/-/raw/$REF/build-aux/cargo-sources.json?inline=false
 
-COMMIT=$(curl "https://gitlab.gnome.org/api/v4/projects/${GITLAB_PROJECT_ID}/repository/commits" | jq -r '.[0].id')
+COMMIT=$(curl "https://gitlab.gnome.org/api/v4/projects/${GITLAB_PROJECT_ID}/repository/commits?ref_name=$REF" | jq -r '.[0].id')
 sed -i "s/commit: main/commit: ${COMMIT}/" app.drey.Warp.yaml
